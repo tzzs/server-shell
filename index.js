031b9a8f31081243b0d6683387b99248e6e7d61a
TZZ <1789803837@qq.com>
2021-11-25 01:21:12 +0800

    ⚙add prettier config and format all files

--- index.js
+++ index.js
@@ -1,65 +1,68 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec')
-const io = require('@actions/io')
-const fs = require('fs')
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const io = require("@actions/io");
+const fs = require("fs");
 
-const values_list = ['PRIVATE_KEY', 'USERNAME', 'IP', 'SHELL']
+const values_list = ["PRIVATE_KEY", "USERNAME", "IP", "SHELL"];
 
 // get input values
-let { PRIVATE_KEY, USERNAME, IP, SHELL } = function () {
-    let values = []
-    values_list.forEach(value => {
-        values[value] = core.getInput(value)
-    })
-    return values
-}()
+let { PRIVATE_KEY, USERNAME, IP, SHELL } = (function () {
+  let values = [];
+  values_list.forEach((value) => {
+    values[value] = core.getInput(value);
+  });
+  return values;
+})();
 
 function create_file(path, content) {
-    fs.appendFile(path, content, (err) => {
-        if (err) throw err;
-    })
+  fs.appendFile(path, content, (err) => {
+    if (err) throw err;
+  });
 }
 
 async function run() {
-    try {
-        if (PRIVATE_KEY.trim().length == 0) {
-            throw "PRIVATE_KEY cannot be empty"
-        }
-        if (USERNAME.trim().length == 0) {
-            throw "USERNAME cannot be empty"
-        }
-        if (IP.trim().length == 0) {
-            throw "IP cannot be empty"
-        }
+  try {
+    if (PRIVATE_KEY.trim().length == 0) {
+      throw "PRIVATE_KEY cannot be empty";
+    }
+    if (USERNAME.trim().length == 0) {
+      throw "USERNAME cannot be empty";
+    }
+    if (IP.trim().length == 0) {
+      throw "IP cannot be empty";
+    }
 
-        // get home dir from environment
-        HOME = process.env['HOME']
-        core.info(HOME)
+    // get home dir from environment
+    const HOME = process.env["HOME"];
+    core.info(HOME);
 
-        // config ssh
-        await io.mkdirP(HOME + "/.ssh")
-        await create_file(HOME + "/.ssh/deploy.key", PRIVATE_KEY);
-        await exec.exec(`ls ${HOME}/.ssh`)
-        await exec.exec(`chmod 600 ${HOME}/.ssh/deploy.key`)
-        await create_file(HOME + "/.ssh/config", `Host server\n\
+    // config ssh
+    await io.mkdirP(HOME + "/.ssh");
+    await create_file(HOME + "/.ssh/deploy.key", PRIVATE_KEY);
+    await exec.exec(`ls ${HOME}/.ssh`);
+    await exec.exec(`chmod 600 ${HOME}/.ssh/deploy.key`);
+    await create_file(
+      HOME + "/.ssh/config",
+      `Host server\n\
     HostName ${IP}\n\
     User ${USERNAME}\n\
     IdentityFile ~/.ssh/deploy.key\n\
-    StrictHostKeyChecking no\n`)
+    StrictHostKeyChecking no\n`
+    );
 
-        await exec.exec("ls -al")
+    await exec.exec("ls -al");
 
-        // check if SHELL is not null
-        if (SHELL.trim().length != 0) {
-            let shells = SHELL.split('\n')
-            for (shell of shells) {
-                core.info(shell)
-                await exec.exec("ssh server " + shell)
-            }
-        }
-    } catch (error) {
-        core.setFailed(error);
+    // check if SHELL is not null
+    if (SHELL.trim().length != 0) {
+      let shells = SHELL.split("\n");
+      for (let shell of shells) {
+        core.info(shell);
+        await exec.exec("ssh server " + shell);
+      }
     }
+  } catch (error) {
+    core.setFailed(error);
+  }
 }
 
 run();
